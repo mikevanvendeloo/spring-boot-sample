060850ef2d1e2618dd604c049e037c5e606f0d38
Mike van Vendeloo <mike@jpoint.nl>
2017-03-21 11:19:34 +0100

    Add test coverage & IT for demo purposes

--- src/test/java/net/vanvendeloo/springboot/SampleSpringBootApplicationIT.java
+++ src/test/java/net/vanvendeloo/springboot/SampleSpringBootApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SampleSpringBootApplicationTests {
+public class SampleSpringBootApplicationIT {
 
 	@Test
 	public void contextLoads() {
